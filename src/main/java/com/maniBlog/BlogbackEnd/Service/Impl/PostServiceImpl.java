22046f458efd3213bdcfc96075e310f0ba570d23
Manish Harit <Manishharit974@gmail.com>
2023-03-24 05:18:50 +0530

    Url getAllPosts updated,  query param for sortBy and sortDir added

--- src/main/java/com/maniBlog/BlogbackEnd/Service/Impl/PostServiceImpl.java
+++ src/main/java/com/maniBlog/BlogbackEnd/Service/Impl/PostServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,8 +30,11 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo,int pageSize) {
-        Pageable pageable = PageRequest.of(pageNo,pageSize);
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
         Page<Post> posts = postRepository.findAll(pageable);
         List<Post> postList = posts.getContent();
 
--- src/main/java/com/maniBlog/BlogbackEnd/Service/PostService.java
+++ src/main/java/com/maniBlog/BlogbackEnd/Service/PostService.java
@@ -8,7 +8,7 @@ import com.maniBlog.BlogbackEnd.PayLoad.PostResponse;
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    PostResponse getAllPosts(int pageNo, int pageSize);
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
     PostDto getPostById(Long id);
 
