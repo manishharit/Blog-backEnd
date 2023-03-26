ae5face588797bcb10f8ccd5634a96d01b38ecc3
Manish Harit <Manishharit974@gmail.com>
2023-03-27 02:10:05 +0530

    Swagger Customization for Post Controller APIs with Annotations

--- src/main/java/com/maniBlog/BlogbackEnd/Controller/PostController.java
+++ src/main/java/com/maniBlog/BlogbackEnd/Controller/PostController.java
@@ -4,7 +4,10 @@ import com.maniBlog.BlogbackEnd.PayLoad.PostDto;
 import com.maniBlog.BlogbackEnd.PayLoad.PostResponse;
 import com.maniBlog.BlogbackEnd.Service.PostService;
 import com.maniBlog.BlogbackEnd.Utils.BlogConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,13 +17,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Tag(
+        name = "POST APIs Collection"
+)
 @RestController
 @RequestMapping("/api/posts")
 @AllArgsConstructor
 public class PostController {
 
     private final PostService postService;
+
+    @Operation(
+            summary = "CREATE POST REST API ----",
+            description = "CREATE POST REST API responsible for creating new posst and saving it to database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -30,6 +44,17 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+
+
+
+    @Operation(
+            summary = "GET ALL POST  REST API ----",
+            description = "GET ALL POST REST API responsible for fetching all posts from  database "
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK"
+    )
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo",defaultValue = BlogConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
@@ -40,10 +65,29 @@ public class PostController {
         return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
     }
 
+
+    @Operation(
+            summary = "GET POST BY ID REST API ----",
+            description = "GET POST BY ID REST API responsible for fetching single post from  database by Id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
+
+
+    @Operation(
+            summary = "UPDATE POST BY ID REST API ----",
+            description = "UPDATE POST BY ID REST API responsible for updating posts onto  database by Id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -52,6 +96,18 @@ public class PostController {
     public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto,@PathVariable(name = "id") Long id){
         return ResponseEntity.ok(postService.updatePostById(postDto,id));
     }
+
+
+
+
+    @Operation(
+            summary = "DELETE POST BY ID REST API ----",
+            description = "DELETE POST BY ID REST API responsible for deleting single post from  database by Id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -61,6 +117,17 @@ public class PostController {
         postService.deletePostById(id);
         return ResponseEntity.ok("Post has been deleted successfully");
     }
+
+
+
+    @Operation(
+            summary = "GET POST BY CATEGORY ID REST API ----",
+            description = "GET POST BY CATEGORY ID REST API responsible for fetching list of post from  database by category Id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK"
+    )
     @GetMapping("/category/{id}")
     public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable(value = "id") Long id){
         List<PostDto> postList = postService.getPostByCategory(id);
