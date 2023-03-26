629c2d6063c8ea8e7ad6703c328b9996b7966946
Manish Harit <Manishharit974@gmail.com>
2023-03-27 01:26:40 +0530

    Swagger documentation basic detailing

--- src/main/java/com/maniBlog/BlogbackEnd/BlogBackEndApplication.java
+++ src/main/java/com/maniBlog/BlogbackEnd/BlogBackEndApplication.java
@@ -1,11 +1,36 @@
 package com.maniBlog.BlogbackEnd;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring-Boot Blog-Rest Api Collection",
+				description = "Rest Api documentation with validations ",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Manish Harit",
+						email = "manishharit909@gmail.com",
+						url = "https://www.youtube.com/c/ManishHarit"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.youtube.com/c/ManishHarit"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog-Backend documentation",
+				url = "https://github.com/ProCoderMani/Blog-backEnd"
+		)
+)
 public class BlogBackEndApplication {
 
 	@Bean
